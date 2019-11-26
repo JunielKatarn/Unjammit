fdd8b9435203ac51a656421f4a9c69463d24c075
Julio C. Rocha <julio@rochsquadron.net>
2019-11-26 13:10:34 -0800

    Define UWP renderer (stub).

---
+++ UWP/Forms/Renderers/UWPHorizontalFillImageRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(Jammit.Forms.Views.HorizontalFillImage), typeof(Jammit.Forms.Renderers.UWPHorizontalFillImageRenderer))]
+namespace Jammit.Forms.Renderers
+{
+  public class UWPHorizontalFillImageRenderer : ImageRenderer
+  {
+
+    #region ViewRenderer overrides
+
+    protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
+    {
+      base.OnElementChanged(e);
+
+      if (Control != null)
+      {
+        //TODO: Make horizontal fill to size and truncate vertically from the bottom up.
+      }
+    }
+
+    #endregion ViewRenderer overrides
+  }
+}
