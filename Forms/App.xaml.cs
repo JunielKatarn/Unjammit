651c1dd73c2b7424bc660f312bf2b373484699d7
Julio C. Rocha <julio@rochsquadron.net>
2020-08-14 23:09:45 -0700

    Move RadioButton_Experimental to forms App.cs

--- Forms/App.xaml.cs
+++ Forms/App.xaml.cs
@@ -27,7 +27,7 @@ namespace Jammit.Forms
     public App()
     {
       //TODO: Remove when Shapes leave experimental state.
-      Device.SetFlags(new string[] { "Shapes_Experimental" });
+      Device.SetFlags(new string[] { "Shapes_Experimental", "RadioButton_Experimental" });
 
       InitializeComponent();
 
