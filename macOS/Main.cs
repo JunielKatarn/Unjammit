312220447507b73218d731d31243440359b680e2
Julio C. Rocha <julio@rochsquadron.net>
2021-06-24 03:18:44 -0700

    Update Hello Mac comment URL

--- macOS/Main.cs
+++ macOS/Main.cs
@@ -7,7 +7,7 @@ namespace Jammit.macOS
     static void Main(string[] args)
     {
       NSApplication.Init();
-      //https://blog.xamarin.com/preview-bringing-macos-to-xamarin-forms/
+      //https://docs.microsoft.com/en-us/xamarin/xamarin-forms/platform/other/mac
       NSApplication.SharedApplication.Delegate = new AppDelegate();
       NSApplication.Main(args);
     }
